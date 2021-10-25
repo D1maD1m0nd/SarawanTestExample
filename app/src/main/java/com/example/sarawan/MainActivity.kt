a14297db6dbb45f2f3a5397b32df794fb50a9921
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2021-10-26 02:16:01 +0500

    add navigation

--- app/src/main/java/com/example/sarawan/MainActivity.kt
+++ app/src/main/java/com/example/sarawan/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.sarawan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.sarawan.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navView: BottomNavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.nav_fragment)
+        navView.setupWithNavController(navController)
     }
 }
