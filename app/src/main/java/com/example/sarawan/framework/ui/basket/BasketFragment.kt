b7b259c60983c162904051e8a1af8f3d470b1777
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2021-10-26 01:05:30 +0500

    fix name

---
+++ app/src/main/java/com/example/sarawan/framework/ui/basket/BasketFragment.kt
@@ -0,0 +1,10 @@
+package com.example.sarawan.framework.ui.basket
+
+
+import androidx.fragment.app.Fragment
+
+class BasketFragment : Fragment() {
+    companion object {
+        fun newInstance() = BasketFragment()
+    }
+}
