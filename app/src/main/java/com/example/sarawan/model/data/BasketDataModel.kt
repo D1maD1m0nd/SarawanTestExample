fe524d9de9ed9d3c72d828519cda91c909239f8f
D1mas <d1m0hkrasav4ik@gmail.com>
2021-11-03 03:01:17 +0500

    add basket model, basket adapter, diffutils basket

--- app/src/main/java/com/example/sarawan/model/data/BasketDataModel.kt
+++ app/src/main/java/com/example/sarawan/model/data/BasketDataModel.kt
@@ -4,10 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 class BasketDataModel(
     @SerializedName("id") val id: Int?,
-    @SerializedName("price") val price: Float?,
-    @SerializedName("name") val itemDescription: String?,
+    @SerializedName("price_per_unit") val price: Float?,
+    @SerializedName("count") val count: Int?,
+    @SerializedName("name") val name: String?,
     @SerializedName("picture") val pictureUrl: String?,
-    @SerializedName("discount") val discount: Int?,
+    @SerializedName("country") val country: String?,
+    @SerializedName("company") val company: String?,
     @SerializedName("shop") val shop: String?,
     @SerializedName("weight") val weight: String?,
 )
