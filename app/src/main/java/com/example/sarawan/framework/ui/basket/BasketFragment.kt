245505df9fd47a738e4be684ee8933fc3954703f
D1mas <d1m0hkrasav4ik@gmail.com>
2021-11-03 03:01:45 +0500

    add view model and impl basket fragment

--- app/src/main/java/com/example/sarawan/framework/ui/basket/BasketFragment.kt
+++ app/src/main/java/com/example/sarawan/framework/ui/basket/BasketFragment.kt
@@ -1,11 +1,22 @@
 package com.example.sarawan.framework.ui.basket
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sarawan.databinding.FragmentBasketBinding
+import com.example.sarawan.databinding.FragmentMainBinding
+import com.example.sarawan.framework.ui.basket.adapter.BasketAdapter
+import com.example.sarawan.framework.ui.basket.adapter.DiffUtilsBasket
+import com.example.sarawan.framework.ui.basket.viewModel.BasketViewModel
 import com.example.sarawan.framework.viewModel.MainViewModel
 import com.example.sarawan.model.data.AppState
+import com.example.sarawan.model.data.BasketDataModel
+import com.example.sarawan.model.data.DataModel
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -13,16 +24,59 @@ class BasketFragment : Fragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-
+    private var _binding: FragmentBasketBinding? = null
+    private val binding get() = _binding!!
+    private val adapter = BasketAdapter()
+    private val viewModel: BasketViewModel by lazy {
+        viewModelFactory.create(BasketViewModel::class.java)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidSupportInjection.inject(this)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBasketBinding.inflate(inflater, container, false)
+        return binding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRcView()
+        viewModel.getStateLiveData().observe(viewLifecycleOwner) { appState ->
+            setState(appState)
+        }
     }
 
+    private fun initRcView() = with(binding) {
+        cardContainerRcView.layoutManager = LinearLayoutManager(context)
+        cardContainerRcView.adapter = adapter
+    }
+    private fun setState(appState: AppState) {
+        when (appState) {
+            is AppState.Success<*> -> {
+                val data = appState.data as List<BasketDataModel>
+                diffData(data)
+            }
+            is AppState.Error -> Unit
+            AppState.Loading -> Unit
+        }
+    }
+
+    private fun diffData(newList : List<BasketDataModel>) {
+        val oldList = adapter.data
+        val utils = DiffUtilsBasket(oldList, newList)
+        val diffResult = DiffUtil.calculateDiff(utils)
+        adapter.setData(newList)
+        diffResult.dispatchUpdatesTo(adapter)
+    }
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
     companion object {
         fun newInstance() = BasketFragment()
     }
---
+++ app/src/main/java/com/example/sarawan/framework/ui/basket/viewModel/BasketViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.sarawan.framework.ui.basket.viewModel
+
+import com.example.sarawan.framework.MainInteractor
+import com.example.sarawan.framework.ui.base.BaseViewModel
+import com.example.sarawan.model.data.AppState
+import com.example.sarawan.rx.ISchedulerProvider
+import io.reactivex.rxjava3.observers.DisposableObserver
+import javax.inject.Inject
+
+class BasketViewModel @Inject constructor(
+    private val interactor: MainInteractor,
+    private val schedulerProvider: ISchedulerProvider
+) : BaseViewModel<AppState>() {
+    private fun getObserver() = object : DisposableObserver<AppState>() {
+        override fun onNext(appState: AppState) {
+            stateLiveData.postValue(appState)
+        }
+
+        override fun onError(e: Throwable) {
+            stateLiveData.postValue(AppState.Error(e))
+        }
+
+        override fun onComplete() = Unit
+    }
+}
