89e96b9355c5881973d2fec209b3cd292af934d4
D1mas <d1m0hkrasav4ik@gmail.com>
2021-11-17 23:50:19 +0500

    commit revert

---
+++ app/src/main/java/com/example/sarawan/framework/ui/main/viewHolder/ButtonHolder.kt
@@ -0,0 +1,25 @@
+package com.example.sarawan.framework.ui.main.viewHolder
+
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sarawan.R
+import com.example.sarawan.databinding.ListItemButtonBinding
+import com.example.sarawan.model.data.DataModel
+
+class ButtonHolder(private val binding: ListItemButtonBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(data: DataModel) {
+        with(binding) {
+            button.text = data.itemDescription
+            root.setBackgroundColor(
+                ContextCompat.getColor(
+                    binding.root.context,
+                    R.color.top_card_background
+                )
+            )
+            button.setOnClickListener {
+
+            }
+        }
+    }
+}
