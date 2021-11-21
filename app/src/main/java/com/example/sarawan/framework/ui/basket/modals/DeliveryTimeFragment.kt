8c163db73b35d6fd3ca88b2c919045f07f6867f0
D1mas <d1m0hkrasav4ik@gmail.com>
2021-11-22 04:25:39 +0500

    add time and date picker dialogs

--- app/src/main/java/com/example/sarawan/framework/ui/basket/modals/DeliveryTimeFragment.kt
+++ app/src/main/java/com/example/sarawan/framework/ui/basket/modals/DeliveryTimeFragment.kt
@@ -5,8 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import com.example.sarawan.R
 import com.example.sarawan.databinding.FragmentDeliveryTimeBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.MaterialTimePicker.INPUT_MODE_KEYBOARD
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
 
 class DeliveryTimeFragment : DialogFragment() {
     private var _binding: FragmentDeliveryTimeBinding? = null
@@ -26,11 +33,63 @@ class DeliveryTimeFragment : DialogFragment() {
             width = WindowManager.LayoutParams.MATCH_PARENT
             height = WindowManager.LayoutParams.MATCH_PARENT
         }
-        binding.backButton.setOnClickListener {
-            dismiss()
-        }
+        initView()
     }
 
+    private fun initView() = with(binding){
+        backButton.setOnClickListener {
+            dismiss()
+        }
+
+        timeImageButton.setOnClickListener{
+            showTimePicker()
+        }
+
+        dateImageButton.setOnClickListener {
+            showDatePicker()
+        }
+    }
+    private fun showDatePicker() {
+        val picker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText(getString(R.string.select_date_delivery))
+                .build()
+
+        // set listener when date is selected
+        picker.addOnPositiveButtonClickListener {
+
+            // Create calendar object and set the date to be that returned from selection
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            calendar.time = Date(it)
+            val date = "${calendar.get(Calendar.DAY_OF_MONTH)}- " +
+                    "${calendar.get(Calendar.MONTH) + 1}-${calendar.get(Calendar.YEAR)}"
+            setDate(date)
+        }
+        picker.show(parentFragmentManager, "DATE PICKER")
+    }
+
+    private fun showTimePicker() {
+        val picker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setInputMode(INPUT_MODE_KEYBOARD)
+                .setTitleText(getString(R.string.select_time_delivery))
+                .build()
+        picker.addOnDismissListener {
+            val hour = picker.hour.toString()
+            val time = picker.minute.toString()
+            setTime(hour, time)
+        }
+        picker.show(parentFragmentManager, "TIME_PICKER")
+    }
+    private fun setDate(date : String) {
+        binding.dateEditText.setText(date, TextView.BufferType.EDITABLE)
+    }
+    private fun setTime(hour : String, minute : String) {
+        val formatHour = if (hour.length > 1) hour else "0${hour}"
+        val formatMinute = if (minute.length > 1) minute else "0${minute}"
+        val time = "$formatHour:$formatMinute"
+        binding.timeEditText.setText(time, TextView.BufferType.EDITABLE)
+    }
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
