9f0bd387bc5f3e4f06a0cc7ef7e6f24d3eafa4f5
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-12-01 23:03:55 +0300

    логика работы с "отправить код" вроде как готова, можно гонять. всегда пока код неверный - чтобы протестировать эту ситуацию

--- app/src/main/java/com/example/sarawan/framework/ui/profile/ProfileCodeFragment.kt
+++ app/src/main/java/com/example/sarawan/framework/ui/profile/ProfileCodeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.sarawan.framework.ui.profile
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +20,8 @@ class ProfileCodeFragment : DialogFragment() {
     private var wrongCode = false
     private val phoneNumber = "+7 000 000 00 00"
 
+    private lateinit var timer: CountDownTimer
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,13 +38,31 @@ class ProfileCodeFragment : DialogFragment() {
         initViews()
         setTitle()
         setCodeBorder()
-        updateTimerText("01:00")
+        updateTimerText(TIMER_INIT_STRING)
+        initTimer()
+    }
+
+    private fun initTimer() {
+        timer = object : CountDownTimer(TIMER_DURATION, TIMER_INTERVAL) {
+            override fun onTick(millis: Long) {
+                val seconds = millis / TIMER_INTERVAL
+                val zero = if (seconds < 10) ZERO else ""
+                updateTimerText("$TWO_ZERO:$zero$seconds")
+            }
+
+            override fun onFinish() = with(binding) {
+                profileCodeRequestAgainButton.isVisible = true
+                profileCodeNoRequestAgainButton.isVisible = false
+                profileCodeTimerTextView.visibility = View.INVISIBLE
+            }
+        }.start()
     }
 
     private fun initViews() = with(binding) {
         profileCodeCloseButton.setOnClickListener { dismiss() }
 
         profileCodeSendButton.setOnClickListener { sendCode() }
+        profileCodeRequestAgainButton.setOnClickListener { sendCodeAgain() }
 
         profileCodeNoSendButton.isVisible = false
         profileCodeRequestAgainButton.isVisible = false
@@ -50,8 +71,32 @@ class ProfileCodeFragment : DialogFragment() {
         profileCodeSentTextView.text = text
     }
 
-    private fun sendCode() {
-        ProfileSuccessFragment.newInstance().show(childFragmentManager, null)
+    private fun sendCodeAgain() = with(binding) {
+        profileCodeRequestAgainButton.isVisible = false
+        profileCodeNoRequestAgainButton.isVisible = true
+        profileCodeTimerTextView.visibility = View.VISIBLE
+
+        profileCodeSendButton.isVisible = true
+        profileCodeNoSendButton.isVisible = false
+
+        wrongCode = false
+        setTitle()
+        setCodeBorder()
+        clearCodeText()
+
+        initTimer()
+    }
+
+    private fun sendCode() = with(binding) {
+        //эта часть проверена, ее потом подключим
+        //ProfileSuccessFragment.newInstance().show(childFragmentManager, null)
+        //а тут мы притворимся, что код вводен неверно
+        profileCodeSendButton.isVisible = false
+        profileCodeNoSendButton.isVisible = true
+
+        wrongCode = true
+        setTitle()
+        setCodeBorder()
     }
 
     private fun updateTimerText(time: String) = with(binding) {
@@ -62,6 +107,15 @@ class ProfileCodeFragment : DialogFragment() {
     private fun setTitle() = with(binding) {
         profileCodeEnterCodeTextView.isVisible = !wrongCode
         profileCodeWrongCodeTextView.isVisible = wrongCode
+    }
+
+    private fun clearCodeText() = with(binding) {
+        profileCodeCode1TextView.setText("")
+        profileCodeCode2TextView.setText("")
+        profileCodeCode3TextView.setText("")
+        profileCodeCode4TextView.setText("")
+        profileCodeCode5TextView.setText("")
+        profileCodeCode6TextView.setText("")
     }
 
     private fun setCodeBorder() = with(binding) {
@@ -80,10 +134,17 @@ class ProfileCodeFragment : DialogFragment() {
 
     override fun onDestroy() {
         super.onDestroy()
+        timer.cancel()
         _binding = null
     }
 
     companion object {
+        private const val TIMER_DURATION = 60_000L
+        private const val TIMER_INTERVAL = 1_000L
+        private const val ZERO = "0"
+        private const val TWO_ZERO = "00"
+        private const val TIMER_INIT_STRING = "01:00"
+
         fun newInstance() = ProfileCodeFragment()
     }
 }
