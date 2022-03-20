b2515249b945785a5c10b082ad3bb6ac2d9a457f
dima <testImplementation>
2022-03-21 00:43:22 +0500

    inc app version

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -3,7 +3,7 @@ object AppConfig {
     const val compileSdk = 32
     const val minSdk = 23
     const val targetSdk = 32
-    const val versionCode = 18
-    const val versionName = "1.18.12"
+    const val versionCode = 19
+    const val versionName = "1.18.13"
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
 }
